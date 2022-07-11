aeccd684310a4148653ac3f73741b86af761756b
truonghqGE <truonghq@galaxy.com.vn>
2022-07-11 13:03:46 +0700

    udpate

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import Headseo from "../components/seo-header";
 import Header from "../components/header";
 import { motion } from 'framer-motion';
 
-function MyApp({ Component, pageProps, infoSettings = {} }) {
+function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const asPath = router.asPath;
 
@@ -19,16 +19,7 @@ function MyApp({ Component, pageProps, infoSettings = {} }) {
       <Headseo />
       <div id="wrapper" className="is-mobile">
         <Header />
-        <motion.main
-          key={router.pathname}
-          variants={variants} // Pass the variant object into Framer Motion
-          initial="hidden" // Set the initial state to variants.hidden
-          animate="enter" // Animated state to variants.enter
-          exit="exit" // Exit state (used later) to variants.exit
-          transition={{ type: "linear", duration: 0.3 }} // Set the transition to linear
-        >
-          <Component {...pageProps} key={asPath} />
-        </motion.main>
+        <Component {...pageProps} key={asPath} />
       </div>
     </>
   );
